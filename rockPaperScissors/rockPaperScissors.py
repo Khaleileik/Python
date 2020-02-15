04ff05c22900c92eb2e119b3a8045f76178032ee
Khaleilei Kidau <Khaleileik@users.noreply.github.com>
2020-02-15 10:35:26 -0600

    rock, paper, scissors game

---
+++ rockPaperScissors/rockPaperScissors.py
@@ -0,0 +1,53 @@
+'''
+Created on Feb 1, 2020
+
+@author: ITAUser
+'''
+import random
+
+keepPlaying = True
+
+while(keepPlaying == True):
+    print("Welcome")
+    print("Type in rock, paper or scissors! First to 2 wins")
+    #rock is 1, paper is 2, scissors is 3
+    
+    scoreComp = 0
+    scorePlayer = 0
+    
+    while(scorePlayer < 2) and (scoreComp < 2):
+        choiceComp = random.randint(1,3)
+        choicePlayer = input("Type in rock, paper, or scissors: ")
+        
+        if(choicePlayer == "q"):
+            keepPlaying = False
+            break
+        elif((choicePlayer == "rock" ) and (choiceComp == 1)) or ((choicePlayer == "paper" ) and (choiceComp == 2)) or ((choicePlayer == "scissors" ) and (choiceComp == 3)):
+            print("DRAW!")
+            print("Computer's Score: ", + scoreComp)
+            print("Your Score: ", + scorePlayer)
+            
+        elif((choicePlayer == "rock" ) and (choiceComp == 2)) or ((choicePlayer == "paper" ) and (choiceComp == 3)) or ((choicePlayer == "scissors" ) and (choiceComp == 1)):
+            print("You lose this round")
+            scoreComp = scoreComp + 1
+            print("Computer's Score: ", + scoreComp)
+            print("Your Score: ", + scorePlayer) 
+        elif((choicePlayer == "rock" ) and (choiceComp == 3)) or ((choicePlayer == "paper" ) and (choiceComp == 1)) or ((choicePlayer == "scissors" ) and (choiceComp == 2)):
+            print("You won this round!")
+            scorePlayer = scorePlayer + 1
+            print("Computer's Score: ", + scoreComp)
+            print("Your Score: ", + scorePlayer)
+        else :
+            print("Input is invalid!")
+            
+    if (scorePlayer == 2):
+            print("You win")
+          
+            
+    if (scoreComp == 2):
+            print("You lost")
+           
+    print("Computer's Score: ", + scoreComp , "Your Score: ", + scorePlayer)
+            
+                   
+            
